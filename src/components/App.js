d52f4660038bdbbbfc673f658794d119b742079f
bibek <bibeksinghthapa@gmail.com>
2018-11-18 12:29:43 -0500

    react practice youtube api video search completed :)

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
 
 
 class App extends React.Component{
@@ -19,7 +20,7 @@ class App extends React.Component{
   }
 
   onVideoSelect = (video) => {
-    console.log('from the app', video)
+    this.setState({selectedVideo:video})
   }
 
 
@@ -28,6 +29,7 @@ class App extends React.Component{
     return(
       <div className = 'container'>
         <SearchBar onFormSubmit={this.onTermSubmit}/>
+        <VideoDetail video={this.state.selectedVideo}/>
         <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
       </div>
 
